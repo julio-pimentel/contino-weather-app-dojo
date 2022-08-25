566393a02ec1f8c8b704b257a7aa4da8e013faf4
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-25 12:16:17 +1000

    Testing deploying gh actions 1

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "s3_bucket" {
 
 output "bucket_name" {
   description = "The name of the bucket"
-  value       = ["${module.s3_bucket.s3_bucket_name}"]
+  value       = ["${module.s3_bucket.s3_bucket_name}"] 
 }
 
 output "bucket_name_arn" {
