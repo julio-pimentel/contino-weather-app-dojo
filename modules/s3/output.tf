666110e9b9c30d2db4c845cb5d1b780a578240c3
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-25 10:23:30 +1000

    Changes in tf file strucutre of modules/s3

---
+++ modules/s3/output.tf
@@ -0,0 +1,10 @@
+### Define Output
+output "s3_bucket_name" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "s3_bucket_name_arn" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.this.arn
+}
