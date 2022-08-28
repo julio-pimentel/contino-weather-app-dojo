eb27f582f279884ea22e3a7d1e2882fa8c465eb0
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-28 15:50:09 +1000

    Update outputs.tf file with correct module names

--- outputs.tf
+++ outputs.tf
@@ -10,25 +10,25 @@ output "bucket_name_arn" {
 
 output "vpc_id" {
   description = "VPC ID"
-  value = module.c04_iac04_vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_1_id" {
   description = "Public Subnet ID for AZ-a"
-  value = module.c04_iac04_vpc.pub_cidr_id_1
+  value = module.vpc.pub_cidr_id_1
 }
 
 output "public_subnet_2_id" {
   description = "Public Subnet ID for AZ-b"
-  value = module.c04_iac04_vpc.pub_cidr_id_2
+  value = module.vpc.pub_cidr_id_2
 }
 
 output "public_subnet_3_id" {
   description = "Public Subnet ID for AZ-c"
-  value = module.c04_iac04_vpc.pub_cidr_id_3
+  value = module.vpc.pub_cidr_id_3
 }
 
 output "alb_dns_name" {
   description = "ALB DNS name"
-  value = module.c04_iac04_alb.alb_dns_name
+  value = module.alb.alb_dns_name
 }
