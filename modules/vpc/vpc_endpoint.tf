e94f642d387e874de2b9056518f5a6ccd72881ff
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-31 14:17:43 +1000

    Debug vpc endpoint 4 apply

--- modules/vpc/vpc_endpoint.tf
+++ modules/vpc/vpc_endpoint.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "s3" {
   vpc_id = aws_vpc.vpc_1.id
-  service_name = "com.amazonaws.us-east-1.gateway-s3"
+  service_name = "com.amazonaws.us-east-1.s3"
   tags = {
     Name = var.vpc_endpoint_name
   }
