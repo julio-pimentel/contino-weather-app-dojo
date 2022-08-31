3779d817665c792c673bd787afa5abd0b126927e
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-31 13:55:07 +1000

    Include 3 public rt debug 2

--- modules/vpc/rt.tf
+++ modules/vpc/rt.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "priv_rt_3" {
 }
 
 # Public route tables 
-resource "aws_route_table" "pub_rt" {
+resource "aws_route_table" "pub_rt_1" {
   vpc_id = aws_vpc.vpc_1.id
 
   route {
@@ -48,7 +48,33 @@ resource "aws_route_table" "pub_rt" {
   }
 
   tags = {
-    Name = var.pub_rt_name
+    Name = var.pub_rt_name_1
+  }
+}
+
+resource "aws_route_table" "pub_rt_2" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.pub_rt_name_2
+  }
+}
+
+resource "aws_route_table" "pub_rt_3" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.pub_rt_name_2
   }
 }
 
@@ -71,15 +97,15 @@ resource "aws_route_table_association" "priv_subnet_3" {
 #Associated public route tables
 resource "aws_route_table_association" "pub_subnet_1" {
   subnet_id = aws_subnet.pub_sub_1.id
-  route_table_id = aws_route_table.pub_rt.id
+  route_table_id = aws_route_table.pub_rt_1.id
 }
 
 resource "aws_route_table_association" "pub_subnet_2" {
   subnet_id = aws_subnet.pub_sub_2.id
-  route_table_id = aws_route_table.pub_rt.id
+  route_table_id = aws_route_table.pub_rt_2.id
 }
 
 resource "aws_route_table_association" "pub_subnet_3" {
   subnet_id = aws_subnet.pub_sub_3.id
-  route_table_id = aws_route_table.pub_rt.id
+  route_table_id = aws_route_table.pub_rt_3.id
 }
