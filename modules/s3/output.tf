5da579072b52e88d796e0bd2eaf222b812eef853
Julio Pimentel <pimentel.julio@outlook.com>
2022-08-31 13:20:07 +1000

    Debugging variable names from s3 buckets

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,10 +1,10 @@
 ### Define Output
 output "s3_bucket_name" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.this.id
+  value       = aws_s3_bucket.s3.id
 }
 
 output "s3_bucket_name_arn" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.this.arn
+  value       = aws_s3_bucket.s3.arn
 }
