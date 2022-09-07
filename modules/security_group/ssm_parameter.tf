35ccce01473120a56a8e7e2311e8beb3e761589e
Julio Pimentel <pimentel.julio@outlook.com>
2022-09-07 15:42:55 +1000

    Debug ssm parameter var 3

--- modules/security_group/ssm_parameter.tf
+++ modules/security_group/ssm_parameter.tf
@@ -1,12 +1,13 @@
 # Security group ID
 resource "aws_ssm_parameter" "alb_sg_id" {
-  name  = "/${var.ssm_resource_prefix}/sg/alb_sg_id"
+  name  = "/${var.ssm_prefix}/sg/alb_sg_id"
   type  = "String"
   value       = aws_security_group.lb_sg.id
 }
 
 resource "aws_ssm_parameter" "ecs_tasks_sg_id" {
-  name  = "/${var.ssm_resource_prefix}/sg/alb_sg_id"
+  name  = "/${var.ssm_prefix
+  }/sg/alb_sg_id"
   type  = "String"
   value       = aws_security_group.ecs_tasks_sg.id
 }
