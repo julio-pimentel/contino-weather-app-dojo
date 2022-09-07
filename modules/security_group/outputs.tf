ea95c3147469d499e447f14ef3215f63e43de9e4
Julio Pimentel <pimentel.julio@outlook.com>
2022-09-07 16:44:00 +1000

    Debug parameter store 8

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,5 +1,5 @@
 # Security group ID
-output "alb_sg_id" {
+output "lb_sg_id" {
   description = "ALB SG ID"
   value       = aws_security_group.lb_sg.id
 }
--- modules/security_group/ssm_parameter.tf
+++ modules/security_group/ssm_parameter.tf
@@ -1,14 +1,14 @@
 # Security group ID
-resource "aws_ssm_parameter" "alb_sg_id" {
-  name  = "/${var.ssm_prefix}/sg/alb_sg_id"
+resource "aws_ssm_parameter" "lb_sg_id" {
+  name  = "/${var.ssm_prefix}/sg/lb_sg_id"
   type  = "String"
   overwrite = true
-  value       = aws_security_group.lb_sg.id
+  value = aws_security_group.lb_sg.id
 }
 
 resource "aws_ssm_parameter" "ecs_tasks_sg_id" {
-  name  = "/${var.ssm_prefix}/sg/alb_sg_id"
+  name  = "/${var.ssm_prefix}/sg/ecs_tasks_sg_id"
   type  = "String"
   overwrite = true
-  value       = aws_security_group.ecs_tasks_sg.id
+  value = aws_security_group.ecs_tasks_sg.id
 }
